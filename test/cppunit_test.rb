33f0b543d08d11f3dd3885da66a5a98cd94dbab4
Ryan Calhoun <ryan.calhoun@wwt.com>
2020-12-02 12:35:09 -0600

    add stages for test and package

--- test/cppunit_test.rb
+++ test/cppunit_test.rb
@@ -6,9 +6,9 @@ class CppUnitTest < Test::Unit::TestCase
   def testCppUnit
     Dir.chdir(File.join(File.dirname(__FILE__), '..', 'build', 'test', configuration.to_s)) {
       output = `./cppunit_test`
-      assert_equal 1, $?.exitstatus
-      assert_match /^[\.FE]+$/, output
-      assert_match /Run:\s+\d+\s+Failures:\s+\d+\s+Errors:\s+\d+/, output
+      assert_equal(1, $?.exitstatus)
+      assert_match(/^[\.FE]+$/, output)
+      assert_match(/Run:\s+\d+\s+Failures:\s+\d+\s+Errors:\s+\d+/, output)
     }
   end
 
@@ -16,8 +16,8 @@ class CppUnitTest < Test::Unit::TestCase
     Dir.chdir(File.join(File.dirname(__FILE__), '..', 'build', 'test', configuration.to_s)) {
       %w(-v --version).each {|opt|
         output = `./cppunit_test #{opt}`
-        assert_equal 0, $?.exitstatus
-        assert_match /CppUnit v/, output
+        assert_equal(0, $?.exitstatus)
+        assert_match(/CppUnit v/, output)
       }
     }
   end
@@ -26,8 +26,8 @@ class CppUnitTest < Test::Unit::TestCase
     Dir.chdir(File.join(File.dirname(__FILE__), '..', 'build', 'test', configuration.to_s)) {
       %w(-h --help).each {|opt|
         output = `./cppunit_test #{opt}`
-        assert_equal 0, $?.exitstatus
-        assert_match /Show this help message/, output
+        assert_equal(0, $?.exitstatus)
+        assert_match(/Show this help message/, output)
       }
     }
   end
@@ -35,8 +35,8 @@ class CppUnitTest < Test::Unit::TestCase
   def testCppUnitBadOption
     Dir.chdir(File.join(File.dirname(__FILE__), '..', 'build', 'test', configuration.to_s)) {
       output, error, status = Open3.capture3 './cppunit_test -X'
-      assert_equal 1, status.exitstatus
-      assert_match /invalid option -X/, error
+      assert_equal(1, status.exitstatus)
+      assert_match(/invalid option -X/, error)
     }
   end
 
@@ -44,9 +44,9 @@ class CppUnitTest < Test::Unit::TestCase
     Dir.chdir(File.join(File.dirname(__FILE__), '..', 'build', 'test', configuration.to_s)) {
       %w(-p --no-print-progress).each {|opt|
         output = `./cppunit_test #{opt}`
-        assert_equal 1, $?.exitstatus
-        assert_no_match /^[\.FE]+$/, output
-        assert_match /Run:\s+\d+\s+Failures:\s+\d+\s+Errors:\s+\d+/, output
+        assert_equal(1, $?.exitstatus)
+        assert_no_match(/^[\.FE]+$/, output)
+        assert_match(/Run:\s+\d+\s+Failures:\s+\d+\s+Errors:\s+\d+/, output)
       }
     }
   end
@@ -55,9 +55,9 @@ class CppUnitTest < Test::Unit::TestCase
     Dir.chdir(File.join(File.dirname(__FILE__), '..', 'build', 'test', configuration.to_s)) {
       %w(-r --no-print-result).each {|opt|
         output = `./cppunit_test #{opt}`
-        assert_equal 1, $?.exitstatus
-        assert_match /^[\.FE]+$/, output
-        assert_no_match /Run:\s+\d+\s+Failures:\s+\d+\s+Errors:\s+\d+/, output
+        assert_equal(1, $?.exitstatus)
+        assert_match(/^[\.FE]+$/, output)
+        assert_no_match(/Run:\s+\d+\s+Failures:\s+\d+\s+Errors:\s+\d+/, output)
       }
     }
   end
@@ -66,9 +66,9 @@ class CppUnitTest < Test::Unit::TestCase
     Dir.chdir(File.join(File.dirname(__FILE__), '..', 'build', 'test', configuration.to_s)) {
       %w(-V --verbose).each {|opt|
         output = `./cppunit_test #{opt}`
-        assert_equal 1, $?.exitstatus
-        assert_match /FooTest::test\w+/, output
-        assert_match /Run:\s+\d+\s+Failures:\s+\d+\s+Errors:\s+\d+/, output
+        assert_equal(1, $?.exitstatus)
+        assert_match(/FooTest::test\w+/, output)
+        assert_match(/Run:\s+\d+\s+Failures:\s+\d+\s+Errors:\s+\d+/, output)
       }
     }
   end
@@ -77,10 +77,10 @@ class CppUnitTest < Test::Unit::TestCase
     Dir.chdir(File.join(File.dirname(__FILE__), '..', 'build', 'test', configuration.to_s)) {
       %w(-x --xml-output).each {|opt|
         output = `./cppunit_test #{opt}`
-        assert_equal 1, $?.exitstatus
-        assert_match /<TestRun>/, output
-        assert_match /<FailedTests>/, output
-        assert_match /<SuccessfulTests>/, output
+        assert_equal(1, $?.exitstatus)
+        assert_match(/<TestRun>/, output)
+        assert_match(/<FailedTests>/, output)
+        assert_match(/<SuccessfulTests>/, output)
       }
     }
   end
@@ -88,25 +88,25 @@ class CppUnitTest < Test::Unit::TestCase
   def testCppUnitByName
     Dir.chdir(File.join(File.dirname(__FILE__), '..', 'build', 'test', configuration.to_s)) {
       output = `./cppunit_test -V FooTest`
-      assert_equal 1, $?.exitstatus
-      assert_match /FooTest/, output
-      assert_no_match /BarTest/, output
-      assert_match /Run:\s+\d+\s+Failures:\s+\d+\s+Errors:\s+\d+/, output
+      assert_equal(1, $?.exitstatus)
+      assert_match(/FooTest/, output)
+      assert_no_match(/BarTest/, output)
+      assert_match(/Run:\s+\d+\s+Failures:\s+\d+\s+Errors:\s+\d+/, output)
 
       output = `./cppunit_test -V BarTest`
-      assert_equal 0, $?.exitstatus
-      assert_no_match /FooTest/, output
-      assert_match /BarTest/, output
-      assert_match /OK\s+\(1\stests\)/, output
+      assert_equal(0, $?.exitstatus)
+      assert_no_match(/FooTest/, output)
+      assert_match(/BarTest/, output)
+      assert_match(/OK\s+\(1\stests\)/, output)
     }
   end
 
   def testCppUnitByMethod
     Dir.chdir(File.join(File.dirname(__FILE__), '..', 'build', 'test', configuration.to_s)) {
       output = `./cppunit_test -V FooTest::testOk`
-      assert_equal 0, $?.exitstatus
-      assert_match /FooTest::testOk/, output
-      assert_match /OK\s+\(1\stests\)/, output
+      assert_equal(0, $?.exitstatus)
+      assert_match(/FooTest::testOk/, output)
+      assert_match(/OK\s+\(1\stests\)/, output)
     }
   end
 
